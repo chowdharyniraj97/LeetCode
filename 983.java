70f13526a66ebd1d41076209758ee9618fb6c12e
NIRAJ CHOWDHARY <chowdharyniraj97@gmail.com>
2020-02-03 09:03:23 -0500

    cleaned the code

--- 983.java
+++ 983.java
@@ -4,14 +4,15 @@
 
 class Solution {
     int global=Integer.MAX_VALUE;
-    HashMap<Integer,Integer>map;
+    Map<Integer,Integer>map;
     public int mincostTickets(int[] days, int[] costs) {
         map=new HashMap<>();
-        return dfs(days,costs,days[0],days[days.length-1],0);
+        return dfs(days,costs,days[0],days[days.length-1]);
    }
     
     
     int findnear(int [] days,int target){// fucntion to find the nearest day to our target in the days array        int val=-1;
+        int val=-1;
         for(int i=0;i<days.length;i++){
             
             if(days[i]>=target)
@@ -21,9 +22,7 @@ class Solution {
         return val;
     }
     
-    int dfs(int days[],int coins[],int start,int end,int sum){
-        
-        int s=0;
+    int dfs(int days[],int coins[],int start,int end){
         int index=findnear(days,start);
         if(index==-1)
             start=end+1;
@@ -39,9 +38,9 @@ class Solution {
             return 0;
         } 
         
-        int a=coins[0]+dfs(days,coins,start+1,end,sum+coins[0]);//adding day 1
-        int b=coins[1]+dfs(days,coins,start+7,end,sum+coins[1]);//adding day 7
-        int c=coins[2]+ dfs(days,coins,start+30,end,sum+coins[2]);// adding day 8
+        int a=coins[0]+dfs(days,coins,start+1,end);//adding day 1
+        int b=coins[1]+dfs(days,coins,start+7,end);//adding day 7
+        int c=coins[2]+ dfs(days,coins,start+30,end);// adding day 8
         int ans=Math.min(a,Math.min(b,c));
         map.put(index,ans);
         return ans;
