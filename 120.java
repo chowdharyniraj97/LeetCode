f1e2ca2c8a93a82caa7473f7919dd2b8e15d8932
NIRAJ CHOWDHARY <chowdharyniraj97@gmail.com>
2020-02-11 10:00:37 -0500

    traingle recursive

--- 120.java
+++ 120.java
@@ -4,27 +4,25 @@ class Solution {
         int rows=triangle.size();
         if(rows<=1)
             return rows==1?triangle.get(0).get(0):0;
-        return triangle.get(0).get(0) + findoptimal(triangle,triangle.get(1),1,rows-1);
+        int a=triangle.get(0).get(0) + findoptimal(triangle,triangle.get(1),1,rows-1,0);
+        int b=triangle.get(0).get(0) + findoptimal(triangle,triangle.get(1),1,rows-1,1);
+        return Math.min(a,b);
         
         
         
     }
     
-    int findoptimal(List<List<Integer>> triangle,List<Integer> list,int level,int maxlevel){
+    int findoptimal(List<List<Integer>> triangle,List<Integer> list,int level,int maxlevel,int ind){
         if (level==maxlevel)
         {
-            int min=Integer.MAX_VALUE;
-            for(int i : list)
-                min=Math.min(min,i);
-            return min;
+            
+            return list.get(ind);
         }
         
-        int m=Integer.MAX_VALUE;
-        for(int i : list){
-            m=Math.min(m,Math.abs(i+findoptimal(triangle,triangle.get(level+1),level+1,maxlevel)));
-            System.out.println(m);
-        }
-        return m;
+        int a=list.get(ind)+findoptimal(triangle,triangle.get(level+1),level+1,maxlevel,ind);
+        if(ind+1<triangle.get(level+1).size())
+           return Math.min(a,list.get(ind)+findoptimal(triangle,triangle.get(level+1),level+1,maxlevel,ind+1));
+        return a;
             
     }
 }
