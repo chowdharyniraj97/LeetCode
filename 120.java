a86d7a6d4a8fe40809c8ab8c858e76a5d1925f79
NIRAJ CHOWDHARY <chowdharyniraj97@gmail.com>
2020-02-11 10:09:40 -0500

    triangle full soln

--- 120.java
+++ 120.java
@@ -1,3 +1,37 @@
+//https://leetcode.com/problems/triangle/
+
+//------------------------recursive-------------------------------------------------------
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        
+        int rows=triangle.size();
+        if(rows<=1)
+            return rows==1?triangle.get(0).get(0):0;
+        int a=triangle.get(0).get(0) + findoptimal(triangle,triangle.get(1),1,rows-1,0);
+        int b=triangle.get(0).get(0) + findoptimal(triangle,triangle.get(1),1,rows-1,1);
+        return Math.min(a,b);
+        
+        
+        
+    }
+    
+    int findoptimal(List<List<Integer>> triangle,List<Integer> list,int level,int maxlevel,int ind){
+        if (level==maxlevel)
+        {
+            
+            return list.get(ind);
+        }
+        
+        int a=list.get(ind)+findoptimal(triangle,triangle.get(level+1),level+1,maxlevel,ind);
+        if(ind+1<triangle.get(level+1).size())
+           return Math.min(a,list.get(ind)+findoptimal(triangle,triangle.get(level+1),level+1,maxlevel,ind+1));
+        return a;
+            
+    }
+}
+
+
+//-=======================================================Memoization==============================================================
 class Solution {
     HashMap<String,Integer> map;
     public int minimumTotal(List<List<Integer>> triangle) {
