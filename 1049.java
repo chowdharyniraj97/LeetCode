a59f17adda2d21151148d0508ae357f3270e9bd8
NIRAJ CHOWDHARY <chowdharyniraj97@gmail.com>
2020-02-11 08:31:47 -0500

    minimum cost to break stones

---
+++ 1049.java
@@ -0,0 +1,22 @@
+//copy this to https://leetcode.com/problems/last-stone-weight-ii/
+
+class Solution {
+    public int lastStoneWeightII(int[] stones) {
+        int S = 0, S2 = 0;
+        for (int s : stones) S += s;
+        int n = stones.length;
+        boolean[][] dp = new boolean[S + 1][n + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[0][i] = true;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int s = 1; s <= S / 2; s++) {
+                if (dp[s][i - 1] || (s >= stones[i - 1] && dp[s - stones[i - 1]][i - 1])) {
+                    dp[s][i] = true;
+                    S2 = Math.max(S2, s);
+                }
+            }
+        }
+        return S - 2 * S2;
+    }
+}
