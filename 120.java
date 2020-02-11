7bbe013f4b66671cc9b6c1444eba4cc79cbf8c77
NIRAJ CHOWDHARY <chowdharyniraj97@gmail.com>
2020-02-11 10:05:25 -0500

    improved one

--- 120.java
+++ 120.java
@@ -1,9 +1,11 @@
 class Solution {
+    HashMap<String,Integer> map;
     public int minimumTotal(List<List<Integer>> triangle) {
         
         int rows=triangle.size();
         if(rows<=1)
             return rows==1?triangle.get(0).get(0):0;
+        map=new HashMap<>();
         int a=triangle.get(0).get(0) + findoptimal(triangle,triangle.get(1),1,rows-1,0);
         int b=triangle.get(0).get(0) + findoptimal(triangle,triangle.get(1),1,rows-1,1);
         return Math.min(a,b);
@@ -13,6 +15,8 @@ class Solution {
     }
     
     int findoptimal(List<List<Integer>> triangle,List<Integer> list,int level,int maxlevel,int ind){
+        if(map.containsKey(level+"|"+ind))
+            return map.get(map.get(level+"|"+ind));
         if (level==maxlevel)
         {
             
@@ -21,7 +25,12 @@ class Solution {
         
         int a=list.get(ind)+findoptimal(triangle,triangle.get(level+1),level+1,maxlevel,ind);
         if(ind+1<triangle.get(level+1).size())
-           return Math.min(a,list.get(ind)+findoptimal(triangle,triangle.get(level+1),level+1,maxlevel,ind+1));
+        {
+             int x= Math.min(a,list.get(ind)+findoptimal(triangle,triangle.get(level+1),level+1,maxlevel,ind+1));
+            map.put(level+"|"+ind,x);
+            return x;
+        }
+        map.put(level+"|"+ind,a);
         return a;
             
     }
