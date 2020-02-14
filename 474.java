5025b7333f6f9d4b542b23eca684c643b9967039
NIRAJ CHOWDHARY <chowdharyniraj97@gmail.com>
2020-02-14 10:45:46 -0500

    fater method of 1s and 0s

--- 474.java
+++ 474.java
@@ -99,3 +99,39 @@ class Solution {
                        return x;
     }
 }
+
+//--------------------------------------DP------==========================-----------=============-==========
+class Solution {
+public int findMaxForm(String[] strs, int m, int n) {
+        if(strs == null || strs.length == 0 || (m == 0 && n == 0)) return 0;
+        int dp [][][] = new int[strs.length][m+1][n+1];
+        int [][] pairs = new int[strs.length][2];
+        for(int i = 0;i<strs.length;i++){
+            for(int j = 0;j<strs[i].length();j++){
+                char ch  = strs[i].charAt(j);
+                if(ch == '0') pairs[i][0]++;
+                else pairs[i][1]++;
+            }
+        }
+        for(int zeros =  pairs[0][0];zeros<=m;zeros++){
+               for(int ones = pairs[0][1];ones<=n;ones++){
+                   dp[0][zeros][ones] = 1;
+               }
+        } 
+        for(int i  = 1;i<strs.length;i++){
+           for(int zeros =  0;zeros<=m;zeros++){
+               for(int ones = 0;ones<=n;ones++){
+                   dp[i][zeros][ones] = dp[i-1][zeros][ones];
+               }
+           }
+           for(int zeros =  pairs[i][0];zeros<=m;zeros++){
+               for(int ones = pairs[i][1];ones<=n;ones++){
+                   dp[i][zeros][ones] = Math.max(dp[i-1][zeros][ones], 1+dp[i-1][zeros-pairs[i][0]][ones-pairs[i][1]]);
+               }
+           }
+        }
+        return dp[strs.length-1][m][n];
+    }
+}
+   
+S
