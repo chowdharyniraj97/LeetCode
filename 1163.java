16ad1b7df286cc73df8d617c83e8c2f08ad8d489
NIRAJ CHOWDHARY <chowdharyniraj97@gmail.com>
2020-02-15 09:52:12 -0500

    O(n) lexi last substring soln

--- 1163.java
+++ 1163.java
@@ -38,3 +38,22 @@ class Solution {
         return findlast(s,ans,target,i+1);
     }
 }
+
+/////////////////////// O(n) solution //////////////////////////////////////////////////////////////////////////
+class Solution {
+    public String lastSubstring(String s) {
+        int i = 0, j = 1, offset = 0, len = s.length();
+        while (i + offset < len && j + offset < len) {
+            char c = s.charAt(i + offset), d = s.charAt(j + offset);
+            if (c == d) {
+                ++offset;
+            }else {
+                if (c < d)  { i += offset + 1; } // chars in [i, ..., i + offset] <= charAt(i) == charAt(j)
+                else { j += offset + 1; } // c > d, chars in [j, ..., j + offset] <= charAt(i) == charAt(j)
+                if (i == j) { ++i; } // avoid duplicate start indices. 
+                offset = 0; // reset offset to 0.
+            }
+        }
+        return s.substring(Math.min(i, j));
+    }
+}
