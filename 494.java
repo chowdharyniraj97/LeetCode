042a67ce96cd130255de77bfdaceb71081a964de
NIRAJ CHOWDHARY <chowdharyniraj97@gmail.com>
2020-03-07 10:20:14 -0500

    TargetSum

--- 494.java
+++ 494.java
@@ -23,3 +23,29 @@ class Solution {
     }
 }
 
+//--------------------------------Top-Down-------------------------------------------
+class Solution {
+    HashMap<String,Integer> map;
+    public int findTargetSumWays(int[] nums, int S) {
+        map=new HashMap<>();
+        return ways(nums,0,S);
+    }
+    
+    int ways(int nums[],int ind,int S){
+        if(ind==nums.length){
+            if(S==0)
+                return 1;
+            return 0;
+        }
+        if(map.containsKey(ind+" "+S))
+            return map.get(ind+" "+S);
+       
+        
+        int plus=0;
+        int neg=0;
+        plus+=ways(nums,ind+1,S+nums[ind]);
+        neg+=ways(nums,ind+1,S-nums[ind]);
+        map.put(ind+" "+S,plus+neg); 
+        return plus+neg;
+    }
+}
