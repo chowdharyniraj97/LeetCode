90947951363b27c9992ac6cfa8c1ace6d1202c17
NIRAJ CHOWDHARY <chowdharyniraj97@gmail.com>
2020-03-13 20:11:07 -0400

    modified merge sorted array

--- 88.java
+++ 88.java
@@ -13,9 +13,7 @@ class Solution {
                 nums1[k--]=nums1[i--];
             }
         }
-        while(i>=0){
-            nums1[k--]=nums1[i--];
-        }
+        
         while(j>=0){
             nums1[k--]=nums2[j--];
         }
