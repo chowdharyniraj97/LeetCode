a9a9039b17f104f7743c880bdf941af20670fff5
NIRAJ CHOWDHARY <chowdharyniraj97@gmail.com>
2020-04-02 09:28:20 -0400

    bestree

--- 1130.java
+++ 1130.java
@@ -1,43 +1,33 @@
 //https://leetcode.com/problems/minimum-cost-tree-from-leaf-values/
 class Solution {
-    HashMap<String,Integer> map;
-    int globalmax=Integer.MAX_VALUE;
     public int mctFromLeafValues(int[] arr) {
-        map=new HashMap<>();
-         return buildtree(arr,0,arr.length-1);
         
-        // return globalmax;
+        int dp[][]=new int[arr.length][arr.length];
+        int len=0;
+        for(len=1;len<=arr.length;len++){
+           // System.out.println(len);
+            for(int i=arr.length-1;i>=0 ;i--){
+                int j=i+len;
+                if(j>=arr.length)
+                    continue;
+               // System.out.println(i+" "+j);
+                dp[i][j]=Integer.MAX_VALUE;
+                for(int k=i;k<j;k++){
+                    int ans=findmax(arr,i,k)*findmax(arr,k+1,j)+dp[i][k]+dp[k+1][j];
+
+                    dp[i][j]=Math.min(dp[i][j],ans);
+                    
+                }
+                
+            }
+        }
+        
+        return dp[0][arr.length-1];
+    
+    
     }
     
-    
-    int buildtree(int []arr, int index,int len ){
-        if(index==len)
-            return 0;
-        if(len-index+1==2)
-            return arr[index]*arr[index+1];
-        if(map.containsKey(index+" "+len))
-            return map.get(index+" "+len);
-        int max=Integer.MAX_VALUE;
-        
-        for(int i=index;i<len;i++){
-            int localmax=0;
-            int lefthalf=buildtree(arr,index,i);
-            int righthalf=buildtree(arr,i+1,len);
-            int leftmax=findmax(arr,index,i);
-            int rightmax=findmax(arr,i+1,len);
-           localmax=lefthalf+righthalf;
-            
-            localmax+=leftmax*rightmax;
-            max=Math.min(localmax,max);
-                
-        }
-       // System.out.println(max + " "+index+" "+len);
-        map.put(index+" "+len,max);
-        return max;
-        
- }
-    
-    int findmax(int arr[],int s,int end){
+     int findmax(int arr[],int s,int end){
         int max=arr[s];
         
         for(int i=s;i<=end;i++)
