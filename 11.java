b46e625a8b9fcfc57eeba156cde341d5a10e91e4
NIRAJ CHOWDHARY <chowdharyniraj97@gmail.com>
2020-05-03 06:59:49 -0400

    11.java

---
+++ 11.java
@@ -0,0 +1,17 @@
+
+class Solution {
+    public int maxArea(int[] height) {
+        int i=0;
+        int j=height.length-1;
+        
+        int max=0;
+        while(i<j){
+            max=Math.max(max,(j-i)*Math.min(height[i],height[j]));
+            if(height[i]<height[j])
+                i++;
+            else
+                j--;
+        }
+        return max;
+    }
+}
