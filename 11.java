57e6ab2b0a64d952ce1832c35f5c9c0298cfa04c
NIRAJ CHOWDHARY <chowdharyniraj97@gmail.com>
2020-05-03 07:00:52 -0400

    11.java modified

--- 11.java
+++ 11.java
@@ -15,3 +15,21 @@ class Solution {
         return max;
     }
 }
+
+// https://leetcode.com/problems/container-with-most-water/
+class Solution {
+    public int maxArea(int[] height) {
+        int i=0;
+        int j=height.length-1;
+        
+        int max=0;
+        while(i<j){
+            max=Math.max(max,(j-i)*Math.min(height[i],height[j]));
+            if(height[i]<height[j])
+                i++;
+            else
+                j--;
+        }
+        return max;
+    }
+}
