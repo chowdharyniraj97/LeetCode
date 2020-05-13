b10423adcfc1e2094f75ebd88dec01a537aa4f56
chwdharyniraj97 <chowdharyniraj97@gmail.com>
2020-05-13 06:28:39 -0400

    odd one out

---
+++ Leetcode_30_day_challenge/May Challenge/week2/singleelement.java
@@ -0,0 +1,10 @@
+
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int n=nums[0];
+        for(int i=1;i<nums.length;i++){
+            n^=nums[i];
+        }
+        return n;
+    }
+}
