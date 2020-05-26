c3a94651066581c7b054a02515385b333e9899aa
Niraj Chowdhary <chowdharyniraj97@gmail.com>
2020-05-26 06:52:07 -0400

    kth smallest

---
+++ Leetcode_30_day_challenge/May Challenge/week3/kthsmallest.java
@@ -0,0 +1,31 @@
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+   public static int ans = 0;
+public int kthSmallest(TreeNode root, int k) {
+    helper(root, k);
+    return ans;
+}
+
+public int helper(TreeNode root, int k) {
+    if (root == null) {
+        return 0;
+    }
+   
+    
+    int leftCount = helper(root.left, k);
+    int rightCount = helper(root.right, k - leftCount - 1);
+    if(k==leftCount+1)
+        ans=root.val;
+    
+    return leftCount + rightCount + 1;
+}
+}
