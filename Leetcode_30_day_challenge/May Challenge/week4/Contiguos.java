9f2aeb0a2dc9955f825216c5143bf8763e3eb969
Niraj Chowdhary <chowdharyniraj97@gmail.com>
2020-05-27 04:50:55 -0400

    1010101

---
+++ Leetcode_30_day_challenge/May Challenge/week4/Contiguos.java
@@ -0,0 +1,24 @@
+
+class Solution {
+    public int findMaxLength(int[] nums) {
+        Map<Integer,Integer> map=new HashMap<>();
+        
+        int globalmax=0;
+        int count=0;
+        map.put(0,-1);
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                count++;
+            }
+            else
+                count--;
+            if(map.containsKey(count)){
+                globalmax=Math.max(i-map.get(count),globalmax);
+            }
+            else
+                map.put(count,i);
+        }
+        
+        return globalmax;
+    }
+}
