4ea84b7f4abe624cb945a7da0238c43fa824c7da
Niraj Chowdhary <chowdharyniraj97@gmail.com>
2020-06-18 10:00:58 -0400

    added comments

--- Leetcode_30_day_challenge/June Challenge/week3/searchbst.java
+++ Leetcode_30_day_challenge/June Challenge/week3/searchbst.java
@@ -2,6 +2,6 @@ class Solution {
   public TreeNode searchBST(TreeNode root, int val) {
     if (root == null || val == root.val) return root;
 
-    return val < root.val ? searchBST(root.left, val) : searchBST(root.right, val);
+    return val < root.val ? searchBST(root.left, val) : searchBST(root.right, val);//checking which side to go and if null is reached we start returning thast
   }
 }
