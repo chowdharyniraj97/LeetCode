ba2bd7d70e168a28fa777edd94285f6e6ee11aa4
Niraj Chowdhary <chowdharyniraj97@gmail.com>
2020-06-25 06:08:31 -0400

    logn soln

--- Leetcode_30_day_challenge/June Challenge/week4/completelTree.java
+++ Leetcode_30_day_challenge/June Challenge/week4/completelTree.java
@@ -1,13 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public int countNodes(TreeNode root) {
         if(root==null)
             return 0;
+        return countlastidx(root,1);
         
-        int left=countNodes(root.left);
-        int right=countNodes(root.right);
-        return left+right+1;
+    }
+    
+    int countlastidx(TreeNode root,int cur){
+        if(root.left==null&&root.right==null)
+            return cur;
+        
+        int left=height(root.left);
+        int right=height(root.right);
+        
+        if(left==right){
+            return countlastidx(root.right,cur*2+1);
+        
+        }
+        else
+            return countlastidx(root.left,cur*2);
+        
+        
+        
     }
     
     
-   
+    
+    int height(TreeNode root){
+        if(root==null)
+            return 0;
+        return 1+height(root.left);
+    }
 }
