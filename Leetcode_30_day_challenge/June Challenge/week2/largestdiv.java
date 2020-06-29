b87a19306f6515381c1922a27dd6d3e6055ce50a
Niraj Chowdhary <chowdharyniraj97@gmail.com>
2020-06-29 08:51:55 -0400

    LArgestdivisible

--- Leetcode_30_day_challenge/June Challenge/week2/largestdiv.java
+++ Leetcode_30_day_challenge/June Challenge/week2/largestdiv.java
@@ -1,66 +1,52 @@
 class Solution {
-    public List<Integer> largestDivisibleSubset(int[] nums) {
-        List<Integer> best=new LinkedList<>();
-        int size=0;
-        Arrays.sort(nums);
-         List<Integer> ans =null;
-        // for(int i=0;i<nums.length;i++){
-            
-            ans = findbest(nums,0,1);
-           // System.out.println(ans);
-            // if(ans.size()>size){
-            //     size=ans.size();
-            //     best=ans;
-        
-        return ans;
-            }
-     
-        
-    
-    
-    List<Integer> findbest(int []nums,int cur,int big){
-        if(cur==nums.length)
-            return new LinkedList<>();
-        
-        List<Integer> x =new LinkedList<>();
-        List<Integer> y =new LinkedList<>();
-        List<Integer> z =new LinkedList<>();
-        List<Integer> xz=new LinkedList<>();
-        int temp=big;
-        
-        if(nums[cur]%big==0||big%nums[cur]==0){
-            big=Math.max(big,nums[cur]);
-            
-            x=findbest(nums,cur+1,big);
-            
-            
-           LinkedList<Integer> p=new LinkedList<>();
-            
-           for (int ele : x)
-               p.add(ele);
-            p.add(nums[cur]);
-            LinkedList<Integer> q=new LinkedList<>();
-            
-            z=findbest(nums,cur+1,temp);            
-           for (int ele : z)
-               q.add(ele);
-            
-            xz=p.size()>q.size() ? p : q;
-            
-            
-            
-        }
-        
-        LinkedList<Integer> r =new LinkedList<>();
-        y=findbest(nums,cur+1,temp);
-        for (int ele : y)
-               r.add(ele);
-        
-        //System.out.println(x+ " "+ y);
-        
-        if(r.size()>xz.size())
-            return r;
-        else
-            return xz;
+  HashMap<Integer, List<Integer>> _eds = new HashMap<Integer, List<Integer>>();
+  int [] _nums = {};
+
+  private List<Integer> EDS(Integer i) {
+    // memoization
+    if (this._eds.containsKey(i)) return this._eds.get(i);
+      
+    List<Integer> maxSubset = new ArrayList();
+    // Find the largest divisible subset of previous elements.
+    for (int k = 0; k < i; ++k) {
+      if (this._nums[i] % this._nums[k] == 0) {
+        List<Integer> subset = EDS(k);
+        if (maxSubset.size() < subset.size()) maxSubset = subset;
+      }    
     }
+    // Extend the found subset with the element itself.
+    List<Integer> newEntry = new ArrayList();
+    newEntry.addAll(maxSubset);
+    newEntry.add(this._nums[i]);
+
+    // update the cached values
+    this._eds.put(i, newEntry);
+    return newEntry;
+  }
+
+  public List<Integer> largestDivisibleSubset(int[] nums) {
+    // Test case with empty set.
+    int n = nums.length;
+    if (n == 0) return new ArrayList();
+        
+    // Container to keep the largest divisible subset
+    //   that ends with each of the nums
+    // key: the index of the element
+    this._eds = new HashMap<Integer, List<Integer>>();
+    
+    /* Sort the original list in ascending order. */
+    Arrays.sort(nums);
+    this._nums = nums;
+    
+    List<Integer> maxSubset = new ArrayList();    
+    /* Calculate the values of all EDS(X_i), 
+       while keeping track of the largest one as the result value. */
+    for (int i = 0; i < n; ++i) {
+      List<Integer> subset = EDS(i);
+      if (maxSubset.size() < subset.size()) maxSubset = subset;
+    }
+    
+    // return the largest one
+    return maxSubset;
+  }
 }
