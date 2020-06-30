417da6da28d30a5ff69ed4c5ce38637c43f9203c
Niraj Chowdhary <chowdharyniraj97@gmail.com>
2020-06-30 06:52:13 -0400

    working soln of largest div

--- Leetcode_30_day_challenge/June Challenge/week2/largestdiv.java
+++ Leetcode_30_day_challenge/June Challenge/week2/largestdiv.java
@@ -1,52 +1,34 @@
 class Solution {
-  HashMap<Integer, List<Integer>> _eds = new HashMap<Integer, List<Integer>>();
-  int [] _nums = {};
-
-  private List<Integer> EDS(Integer i) {
-    // memoization
-    if (this._eds.containsKey(i)) return this._eds.get(i);
-      
-    List<Integer> maxSubset = new ArrayList();
-    // Find the largest divisible subset of previous elements.
-    for (int k = 0; k < i; ++k) {
-      if (this._nums[i] % this._nums[k] == 0) {
-        List<Integer> subset = EDS(k);
-        if (maxSubset.size() < subset.size()) maxSubset = subset;
-      }    
-    }
-    // Extend the found subset with the element itself.
-    List<Integer> newEntry = new ArrayList();
-    newEntry.addAll(maxSubset);
-    newEntry.add(this._nums[i]);
-
-    // update the cached values
-    this._eds.put(i, newEntry);
-    return newEntry;
-  }
-
   public List<Integer> largestDivisibleSubset(int[] nums) {
     // Test case with empty set.
     int n = nums.length;
     if (n == 0) return new ArrayList();
         
     // Container to keep the largest divisible subset
-    //   that ends with each of the nums
-    // key: the index of the element
-    this._eds = new HashMap<Integer, List<Integer>>();
-    
+    //   that ends with each of the nums.
+    List<ArrayList> EDS = new ArrayList();
+    for (int num : nums) EDS.add(new ArrayList());
+
     /* Sort the original list in ascending order. */
     Arrays.sort(nums);
-    this._nums = nums;
-    
-    List<Integer> maxSubset = new ArrayList();    
-    /* Calculate the values of all EDS(X_i), 
-       while keeping track of the largest one as the result value. */
+
+    /* Calculate all the values of EDS(X_i) */
     for (int i = 0; i < n; ++i) {
-      List<Integer> subset = EDS(i);
-      if (maxSubset.size() < subset.size()) maxSubset = subset;
+      List<Integer> maxSubset = new ArrayList();
+            
+      // Find the largest divisible subset of previous elements.
+      for (int k = 0; k < i; ++k) 
+        if (nums[i] % nums[k] == 0 && maxSubset.size() < EDS.get(k).size())
+          maxSubset = EDS.get(k);
+          
+      // Extend the found subset with the element itself.
+      EDS.get(i).addAll(maxSubset);
+      EDS.get(i).add(nums[i]);
     }
-    
-    // return the largest one
-    return maxSubset;
-  }
+    /* Find the largest of EDS values  */
+    List<Integer> ret = new ArrayList();
+    for (int i = 0; i < n; ++i) 
+      if (ret.size() < EDS.get(i).size()) ret = EDS.get(i);
+    return ret;
+  }  
 }
